57e1dddce358b0596369430bcc46a57056c10702
Leandro Pedrosa Rodrigues <pedrosalpr@gmail.com>
2024-01-23 02:03:14 -0300

    feat(user): add softdeletes in migration and model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
